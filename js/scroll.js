858d3c8e75661d18b1dc8c2a6bfffd94dac8f02b
ghrayri <ghrayric@gmail.com>
2023-07-06 00:04:31 +0100

    Initial commit

---
+++ js/scroll.js
@@ -0,0 +1,13 @@
+<script>
+function goto(pageid) {
+    var page = document.getElementById(pageid);
+    if (page ==="register") {
+        location.href = "#register";
+      
+    }
+    else {
+        location.href = "#donate";
+    }
+  }
+  
+  </script>
